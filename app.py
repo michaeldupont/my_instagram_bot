4ce9f549b9168baaeab2a2209405cdc0f4547e51
Michael <michael@homedupont.fr>
2019-04-10 11:38:46 +0200

    indent error handling

--- app.py
+++ app.py
@@ -6,28 +6,29 @@ MyApp = Flask(__name__)
 
 @MyApp.route("/")
 def hello():
-	
 	return render_template('index.html')
 
 @MyApp.route('/data')
 def data():
 	with open('config.json', 'r') as f:
-    	config = json.load(f)
-
+		config = json.load(f)
+	
 	user = config['INSTAGRAM']['USER']
 	pwd = config['INSTAGRAM']['PASSWORD']
 
-
 	api = InstagramAPI(user, pwd)
+	
 	if (api.login()):
-        	api.getSelfUserFeed()  # get self user feed
-        	data = api.LastJson
-        	print (type(data))
-        	json.dumps(data, indent=4)
-        	with open('data1.json', 'w') as f:
-                	f.write(json.dumps(data, indent=4))
+		api.getSelfUserFeed()  # get self user feed
+		data = api.LastJson
+        print (type(data))
+    	json.dumps(data, indent=4)
+       	with open('data1.json', 'w') as f:
+			f.write(json.dumps(data, indent=4))
+	
 	else:
-        	print("Can't login!")
+		print("can't login")
+	
 	return render_template('index.html')
 
 @MyApp.route('/bytel')
